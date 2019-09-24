350f7a76607145caa3d2b4a6137602be78bd3872
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-09-24 20:02:07 +0200

    Initial commit

---
+++ Console/Program.fs
@@ -0,0 +1,59 @@
+﻿// Learn more about F# at http://fsharp.org
+open FSharpPlus
+open System.Linq
+open System.IO
+open System
+
+[<Diagnostics.CodeAnalysis.SuppressMessage("*", "EnumCasesNames")>]
+type Cmd=
+  |fetch=0
+  |batch=1
+type CmdArgs =
+  { Command: Cmd option; Dir: string }
+let (|Cmd|_|) : _-> Cmd option = tryParse
+[<EntryPoint>]
+let main argv =
+  let defaultArgs = { Command = None; Dir = Directory.GetCurrentDirectory() }
+  let usage =
+   ["Usage:"
+    sprintf "    --dir     DIRECTORY  where to store data (Default: %s)" defaultArgs.Dir
+    sprintf "    COMMAND    one of [%s]" (Enum.GetValues( typeof<Cmd> ).Cast<Cmd>() |> Seq.map string |> String.concat ", " )]
+    |> String.concat Environment.NewLine
+  let rec parseArgs b args =
+    match args with
+    | [] -> Ok b
+    | "--dir" :: dir :: xs -> parseArgs { b with Dir = dir } xs
+    | Cmd cmd :: xs-> parseArgs { b with Command = Some cmd } xs
+    | invalidArgs ->
+      sprintf "error: invalid arguments %A" invalidArgs |> Error
+
+  match argv |> List.ofArray |> parseArgs defaultArgs with
+  | Ok args->
+    let runSynchronouslyAndPrintResult fn=
+      match Async.RunSynchronously fn  with
+        | Ok v->
+          Console.WriteLine (string v)
+          0
+        | Error e ->
+          Console.Error.WriteLine (string e)
+          1
+
+    match args with
+    | { Dir=dir; Command=Some command } ->
+      match command with
+      | Cmd.fetch ->
+      
+        //Async.RunSynchronously( )
+        0
+      | Cmd.batch ->
+
+        //Async.RunSynchronously( )
+        0
+    | _ ->
+      printfn "error: Expected command"
+      printfn "%s" usage
+      1
+  | Error err->
+      printfn "%s" err
+      printfn "%s" usage
+      1
---
+++ Lib/Library.fs
@@ -0,0 +1,34 @@
+﻿namespace Lib
+open FSharpPlus
+open FSharpPlus.Operators
+//open Ply
+open FSharp.Control.Tasks.Builders
+open Fleece.Newtonsoft
+open Fleece.Newtonsoft.Operators
+open FSharp.Data
+open FSharp.Data.HttpRequestHeaders
+
+type Person = {
+    Name: string
+    Age: int
+    Children: Person list
+}
+
+type Person with
+    static member Create name age children = { Person.Name = name; Age = age; Children = children }
+
+    static member OfJson json =
+        match json with
+        | JObject o -> Person.Create <!> (o .@ "name") <*> (o .@ "age") <*> (o .@ "children")
+        | x -> Decode.Fail.objExpected x
+
+module Say =
+  let postThing()=
+    Http.RequestString
+      ( "http://httpbin.org/post", 
+        headers = [ ContentType HttpContentTypes.Json ],
+        body = TextRequest """ {"test": 42} """)
+
+  let parseJson(str:string) : Person ParseResult=parseJson str
+  let hello name =
+    printfn "Hello %s" name
