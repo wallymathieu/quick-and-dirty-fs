8560a09e0f3b352dcfdcfa40f4bca287f9b3e942
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-09-24 20:13:45 +0200

    More samples

--- Lib/Library.fs
+++ Lib/Library.fs
@@ -1,19 +1,35 @@
 ﻿namespace Lib
 open FSharpPlus
 open FSharpPlus.Operators
-//open Ply
-open FSharp.Control.Tasks.Builders
+open FSharp.Control.Tasks.Builders //Ply
 open Fleece.Newtonsoft
 open Fleece.Newtonsoft.Operators
 open FSharp.Data
 open FSharp.Data.HttpRequestHeaders
+open System.Threading.Tasks
+// useful for quick and dirty integration work:
+type Somedoc= JsonProvider<"./somedoc.json">
+
+type ISomeTaskBased =
+    abstract member Add: int-> Task<int>
 
 type Person = {
     Name: string
     Age: int
     Children: Person list
 }
-
+type Country =
+| NotSpecified
+| England
+| Wales
+| Scotland
+| NorthernIreland
+ with static member op_Implicit(c:Country) = 
+    match c with | NotSpecified    -> 0
+                 | England         -> 1
+                 | Wales           -> 2
+                 | Scotland        -> 3
+                 | NorthernIreland -> 4
 type Person with
     static member Create name age children = { Person.Name = name; Age = age; Children = children }
 
@@ -32,3 +48,7 @@ module Say =
   let parseJson(str:string) : Person ParseResult=parseJson str
   let hello name =
     printfn "Hello %s" name
+  let doTaskBasedStuff(taskb:ISomeTaskBased, i:Country)=task{
+    let! v= taskb.Add(implicit i)
+    return v+1
+  }
